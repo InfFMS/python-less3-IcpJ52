0fa3deb8548438a9ce37a02a8fae80c1429f4c0b
IcpJ42 <iblion08@gmail.com>
2024-10-07 13:14:21 +0700

    Сделал 3 задание

--- task3.py
+++ task3.py
@@ -1,4 +1,22 @@
 # с клавиатуры вводятся числа, ввод завершается числом 0.
 # Определить, сколько было введено простых натуральных чисел
 # (которые делятся только сами на себя и на 1), и сколько составных.
-
+prime = 0
+composite = 0
+while True:
+    n = float(input())
+    if n == 0:
+        print(f"Простых чисел: {prime}")
+        print(f"Составных чисел: {composite}")
+        break
+    if n == int(n):
+        if n == 1:
+            continue
+        is_prime = 1
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                composite += 1
+                is_prime = 0
+                break
+        if is_prime == 1:
+            prime += 1
