fcc190f7a59a132fffd1bd7ab74c6adb696be44f
IcpJ42 <iblion08@gmail.com>
2024-10-07 12:54:13 +0700

    Переделал задачу 1

--- task1.py
+++ task1.py
@@ -4,7 +4,7 @@
 neg = 0
 pos = 0
 while True:
-    n = int(input())
+    n = float(input())
     if n == 0:
         print(f"Положительных чисел: {pos}")
         print(f"Отрицательных чисел: {neg}")
